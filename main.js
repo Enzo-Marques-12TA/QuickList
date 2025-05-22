d2a63dc0ff1f81c10df681660feae6ef1b7b23d1
EA - Enzo Marques Araujo <marques_araujo@educadventista.org>
2025-05-22 13:31:24 -0300

    validação do preenchimento da input para criação de um novo item

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ const items = []
 function addItem (){
     const itemName  = document.querySelector("#item").value
 
+    if (itemName === ""){
+        alert("Digite um item válido!")
+        return
+    }
+
 //objeto
 const item = {
     name: itemName,
